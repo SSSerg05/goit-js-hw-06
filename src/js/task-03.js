42a2cc9b6a18fd606a3a5cdba4660c2bce7fca62
SSSerg05 <ssserg05@gmail.com>
2023-03-17 16:33:35 +0200

    таск 3

--- src/js/task-03.js
+++ src/js/task-03.js
@@ -1,3 +1,9 @@
+// Используй массив объектов images для создания элементов <img> вложенных в <li>. 
+// Для создания разметки используй шаблонные строки и метод insertAdjacentHTML().
+
+// Все элементы галереи должны добавляться в DOM за одну операцию вставки.
+// Добавь минимальное оформление галереи флексбоксами или гридами через CSS классы.
+
 const images = [
   {
     url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
@@ -12,3 +18,60 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+//знайшли заданий елемент
+const list = document.querySelector('.gallery');
+
+// задаємо йому стилі для ul
+const listStyle = {
+  'display': 'flex',
+  'flex-wrap': 'wrap',
+  'row-gap': '40px',
+  'column-gap': '20px',
+  'list-style': 'none',
+  'padding-left': '15px',
+  'padding-right': '15px',
+  'margin': '0 auto',
+  'width': '100%',
+}
+// замінюємо стилі у елемента
+Object.assign(list.style, listStyle);
+
+//задаємо змінну контейнер, де будемо зберігати блок елементів 
+const documentFragment = document.createDocumentFragment();
+
+// додаємо картинки у елементах списку
+images.forEach(item => { 
+  documentFragment.appendChild( createNewItem(item) );
+})
+
+// об'єднуємо всі елементи
+list.appendChild(documentFragment);
+
+/*
+* @param {url: {String}, alt: {String}}    {Object} 
+* @return li {Node}
+*/
+// робимо деструктурізацію об'єкту з атрибутами картинки
+function createNewItem({ url, alt }) { 
+  const li = document.createElement('li');
+  const img = document.createElement('img');
+  img.src = url;
+  img.alt = alt;
+
+  // стилі для картинки
+  const imgStyle = {
+    'display': 'block',
+    'max-width': '100%',
+    'height': 'auto',
+    'border': '1px solid black',
+    'border-radius': '20px',
+    'box-shadow': '0px 1px 6px rgba(46, 47, 66, 0.96)',
+  }
+  // змінюємо стилі у картинок
+  Object.assign(img.style, imgStyle);
+ 
+  // створюємо блок елеменів li > img
+  li.appendChild(img);
+  return li;
+}
