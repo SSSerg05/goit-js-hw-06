3ff39a512b2ede7de6ea6a60b0f704b14deb85af
SSSerg05 <ssserg05@gmail.com>
2023-03-20 16:53:13 +0200

    task10

--- js/task-10.js
+++ js/task-10.js
@@ -1,15 +1,18 @@
-// Напиши скрипт создания и очистки коллекции элементов. Пользователь вводит 
-// количество элементов в input и нажимает кнопку Создать, после чего рендерится 
+// Напиши скрипт создания и очистки коллекции элементов. Пользователь вводит
+// количество элементов в input и нажимает кнопку Создать, после чего рендерится
 // коллекция. При нажатии на кнопку Очистить, коллекция элементов очищается.
 
-// Создай функцию createBoxes(amount), которая принимает один параметр - число. 
-// Функция создает столько < div >, сколько указано в amount и добавляет их в 
+// Создай функцию createBoxes(amount), которая принимает один параметр - число.
+// Функция создает столько < div >, сколько указано в amount и добавляет их в
 // div#boxes.
 
 // Размеры самого первого <div> - 30px на 30px.
 // Каждый элемент после первого, должен быть шире и выше предыдущего на 10px.
-// Все элементы должены иметь случайный цвет фона в формате HEX. Используй 
+// Все элементы должены иметь случайный цвет фона в формате HEX. Используй
 // готовую функцию getRandomHexColor для получения цвета.
+
+// Создай функцию destroyBoxes(), которая очищает содержимое div#boxes, 
+// тем самым удаляя все созданные элементы.
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
@@ -29,7 +32,7 @@ let counterValue = 0;
 
 input.addEventListener('change', () => { counterValue = Number(input.value) });
 btnCreate.addEventListener('click', () => { createBoxes(counterValue) });
-btnClear.addEventListener('click', () => { clearBoxes(counterValue) });
+btnClear.addEventListener('click', () => { destroyBoxes(counterValue) });
 
 /*  додаємо до батьківського елементу n-кількість елементів
 *
@@ -65,7 +68,7 @@ function createBoxes(amount) {
 * @param counterValue - кількість елементів які видаляємо
 * @return false якщо немає чого видаляти, true - видалено задану кількість елементів
 */
-function clearBoxes(counterValue) { 
+function destroyBoxes(counterValue) { 
   
   // перевірка на кількість елементів у батьковькому контейнері
   let counter = counterValue;
